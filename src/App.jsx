969c4531a170ed903a54f6634ff6a7922c6517ca
Zubair Sidhu <sidhuzubair@gmail.com>
2021-10-29 23:45:03 -0700

    Husky test eslint warns

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var a =    5;
+  var a =    0;
   return (
     <div className="App">
       <header className="App-header">
