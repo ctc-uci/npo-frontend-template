8b016c85a7d902afb5804e8ad2393e7f68b70a67
Kaitlin Leung <leungkn@uci.edu>
2024-07-18 19:34:51 -0700

    css changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import chair from '../../images/chair-star-circle.png';
 import classes from './Profile.module.css';
 
@@ -24,54 +19,61 @@ function Profile() {
       </p>
       <img src={chair} alt="chair surrounded by a circle of stars" className={classes.chair} />
 
-      <Container className={classes.verticalContainer}>
-        <Row className={classes.r1}>
-          <Form.Control placeholder="Student's First Name" />
-          <Form.Control placeholder="Student's Middle Name" />
-          <Form.Control placeholder="Student's Last Name" />
-        </Row>
-        <Row className={classes.r2}>
-          <Form.Control placeholder="Student's First Chair Instrument" style={{ width: '65%' }} />
-          <Button style={{ width: '30%' }}>Upload Profile Picture</Button>
-        </Row>
-        <Form.Control placeholder="Student's Email Address" />
-        <Row style={{ marginBottom: '0' }}>
-          <Form.Control placeholder="Name of School" style={{ width: '65%' }} />
-          <Form.Control placeholder="Graduation Year" style={{ width: '30%' }} />
-        </Row>
-        <Row style={{ position: 'relative', height: '2%' }}>
+      <div className={classes.verticalContainer}>
+        <div className={classes.r1}>
+          <input type="text" placeholder="Student's First Name" />
+          <input type="text" placeholder="Student's Middle Name" />
+          <input type="text" placeholder="Student's Last Name" />
+        </div>
+        <div className={classes.r2}>
+          <input
+            type="text"
+            placeholder="Student's First Chair Instrument"
+            style={{ width: '65%' }}
+          />
+          <button style={{ width: '30%' }} type="button">
+            Upload Profile Picture
+          </button>
+        </div>
+        <input type="text" placeholder="Student's Email Address" />
+        <div style={{ marginBottom: '0' }}>
+          <input type="text" placeholder="Name of School" style={{ width: '65%' }} />
+          <input type="text" placeholder="Graduation Year" style={{ width: '30%' }} />
+        </div>
+        <div style={{ position: 'relative', height: '2%' }}>
           <p style={{ position: 'absolute', top: '5%', right: '15%', marginTop: '0' }}>
             (High School Only)
           </p>
-        </Row>
-        <Row>
-          <Form.Control placeholder="School's Address" style={{ width: '35%' }} />
-          <Form.Control placeholder="City" style={{ width: '25%' }} />
-          <Form.Control placeholder="State" />
-          <Form.Control placeholder="Zip" />
-        </Row>
+        </div>
+        <div>
+          <input type="text" placeholder="School's Address" style={{ width: '35%' }} />
+          <input type="text" placeholder="City" style={{ width: '25%' }} />
+          <input type="text" placeholder="State" />
+          <input type="text" placeholder="Zip" />
+        </div>
         {[...Array(3)].map(value => (
           <div
             key={value}
             style={{ display: 'flex', flexDirection: 'column', height: '22vh', marginBottom: '0%' }}
           >
-            <Row style={{ position: 'relative', height: '2%', marginTop: '3%' }}>
+            <div style={{ position: 'relative', height: '2%', marginTop: '3%' }}>
               <p style={{ position: 'absolute', bottom: '10%', left: '0%', marginBottom: '0' }}>
                 First Chair/First Section - seven or more consecutive weeks
               </p>
-            </Row>
-            <Row className={classes.r3}>
-              <Form.Control
+            </div>
+            <div className={classes.r3}>
+              <input
+                type="text"
                 placeholder="Name of School Performance Ensemble"
                 style={{ width: '65%' }}
               />
-              <Form.Control placeholder="Year" style={{ width: '10%' }} />
-              <Form.Control placeholder="School Grade" style={{ width: '15%' }} />
-            </Row>
-            <Row className={classes.red}>
-              <Form.Control placeholder="Director's First Name" style={{ width: '35%' }} />
-              <Form.Control placeholder="Director's Last Name" style={{ width: '35%' }} />
-            </Row>
+              <input type="text" placeholder="Year" style={{ width: '10%' }} />
+              <input type="text" placeholder="School Grade" style={{ width: '15%' }} />
+            </div>
+            <div className={classes.red}>
+              <input type="text" placeholder="Director's First Name" style={{ width: '35%' }} />
+              <input type="text" placeholder="Director's Last Name" style={{ width: '35%' }} />
+            </div>
           </div>
         ))}
         {[...Array(5)].map(value => (
@@ -79,17 +81,21 @@ function Profile() {
             key={value}
             style={{ display: 'flex', flexDirection: 'column', height: '22vh', marginBottom: '0%' }}
           >
-            <Row style={{ position: 'relative', height: '2%', marginTop: '3%' }}>
+            <div style={{ position: 'relative', height: '2%', marginTop: '3%' }}>
               <p style={{ position: 'absolute', bottom: '10%', left: '0%', marginBottom: '0' }}>
                 First Chair/First Section
               </p>
-            </Row>
-            <Row className={classes.r3}>
-              <Form.Control placeholder="Name of Honor Ensemble" style={{ width: '65%' }} />
-              <Form.Control placeholder="Year" style={{ width: '10%' }} />
-              <Form.Control placeholder="School Grade" style={{ width: '15%' }} />
-            </Row>
-            <Form.Control placeholder="Name of Sponsoring Association" className={classes.r3} />
+            </div>
+            <div className={classes.r3}>
+              <input type="text" placeholder="Name of Honor Ensemble" style={{ width: '65%' }} />
+              <input type="text" placeholder="Year" style={{ width: '10%' }} />
+              <input type="text" placeholder="School Grade" style={{ width: '15%' }} />
+            </div>
+            <input
+              type="text"
+              placeholder="Name of Sponsoring Association"
+              className={classes.r3}
+            />
           </div>
         ))}
         {[...Array(2)].map(value => (
@@ -97,26 +103,28 @@ function Profile() {
             key={value}
             style={{ display: 'flex', flexDirection: 'column', height: '17vh', marginBottom: '0%' }}
           >
-            <Row className={classes.red}>
-              <Form.Control placeholder="Parent's First Name" style={{ width: '30%' }} />
-              <Form.Control placeholder="Parent's Middle Name" style={{ width: '30%' }} />
-              <Form.Control placeholder="Parent's Last Name" style={{ width: '30%' }} />
-            </Row>
-            <Row className={classes.red}>
-              <Form.Control placeholder="Parent's Email Address" style={{ width: '65%' }} />
-              <Form.Control placeholder="Parent's Phone Number" style={{ width: '30%' }} />
-            </Row>
+            <div className={classes.red}>
+              <input type="text" placeholder="Parent's First Name" style={{ width: '30%' }} />
+              <input type="text" placeholder="Parent's Middle Name" style={{ width: '30%' }} />
+              <input type="text" placeholder="Parent's Last Name" style={{ width: '30%' }} />
+            </div>
+            <div className={classes.red}>
+              <input type="text" placeholder="Parent's Email Address" style={{ width: '65%' }} />
+              <input type="text" placeholder="Parent's Phone Number" style={{ width: '30%' }} />
+            </div>
           </div>
         ))}
-        <Row>
-          <Button className={classes.button}>Upload Profile</Button>
+        <div>
+          <button className={classes.button} type="button">
+            Upload Profile
+          </button>
           <p style={{ margin: '0 0 0 5%' }}>
             {`Upon uploading your profile, you will be included in the First Chair American National
             Registry. Clicking on your name will direct you to your profile page, and there's a
             chance your profile may be showcased on the homepage as a featured profile.`}
           </p>
-        </Row>
-      </Container>
+        </div>
+      </div>
     </div>
   );
 }
